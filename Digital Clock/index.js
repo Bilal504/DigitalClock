bfd990201328b565af01dd18cf4034188f6b9aa3
Syed Bilal <72281240+Bilal504@users.noreply.github.com>
2022-08-13 00:37:01 +0530

    Formatted the code

--- Digital Clock/index.js
+++ Digital Clock/index.js
@@ -3,32 +3,32 @@ const minuteEI = document.getElementById("minute");
 const secondEI = document.getElementById("second");
 const ampmEI = document.getElementById("ampm");
 
-function updateClock(){
+function updateClock() {
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
 
     let ampm = "AM";
-    if(h>12){
+    if (h > 12) {
         h -= 12;
         ampm = "PM";
     }
 
-    h = h<10? "0" + h : h;
-    m = m<10? "0" + m : m;
-    s = s<10? "0" + s : s;
-    
-    
-        hourEI.innerText= h;
-    
-        minuteEI.innerText= m;
-    
-        secondEI.innerText= s;
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
 
-        ampmEI.innerText = ampm
 
-        setTimeout(()=>{
-            updateClock()
-        }, 1000);
+    hourEI.innerText = h;
+
+    minuteEI.innerText = m;
+
+    secondEI.innerText = s;
+
+    ampmEI.innerText = ampm
+
+    setTimeout(() => {
+        updateClock()
+    }, 1000);
 }
 updateClock();
